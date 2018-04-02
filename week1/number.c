5e896bd4e2173c9ff956534c02e359796e0bec23
Anuradha <sangkeerthana70@gmail.com>
2018-04-02 17:47:00 +0000

    Week 0 Lecture added

---
+++ week1/number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h>
+
+//int stuff(void)
+//{
+//	print("stuff\n")
+//	return 1;
+//}
+
+//int stuffs(char* stuff)
+//{
+//	printf("%s\n", stuff);
+//	return 1;
+//}
+
+int main(void)
+{
+    printf("Hello, World\n");
+    int integer = 5 + 8;
+   // binary  000101 + 001000 = 000000;
+   //000101 +
+  // 001000 = 001101
+    printf("Here's my number: %i\n", integer);
+    //float flt = 4.1 + 3.2;
+    float flt = 5.3 - 4.83;
+    printf("Here's my float: %.20f\n", flt);
+}
